ef434430f366e7b476b7383ccaa6a485354f8103
ouskah <ouskah.gn@gmail.com>
2016-01-26 01:17:57 +0100

    Ajout button & function 'Annuler'

--- tableaux.js
+++ tableaux.js
@@ -1,26 +1,37 @@
 $liste = document.getElementById("liste");
 $ajouter = document.getElementById("ajouter");
+$annuler = document.getElementById("annuler");
 $saisie = document.getElementById("saisie");
 
-tableau = [];
+tableau = []; // variable tableau avec accès global
 
-function afficherListe() {
+function afficherListe() { // afficher la liste des éléments ajouter
 
     elements = "";
 
     for (i=0; i < tableau.length; i++) {
         elements += "<li>" + tableau[i] + "</li>";
     }
-    if (tableau.length !== ""){ // si aucune saisie ne pas afficher un <li> vide
-        $liste.innerHTML = elements;
-    }
+        $liste.innerHTML = elements; 
 }
 
-function ajouter() {
+function ajouter() { // ajout d'un élément à la liste
     tableau.push($saisie.value);
     afficherListe();
-    $saisie.value = "";
+    $saisie.value = " "; // effacer la zone de saisie après l'ajout
 }
 
-$ajouter.onclick = ajouter;
-afficherListe();
+
+function annuler() { // annuler le dernier élément ajouté
+    tableau.pop(); // supprimer le dernier élément du tableau
+    afficherListe(); // afficher la nouvelle liste
+}
+
+
+
+$ajouter.onclick = ajouter; // ajout d'un élément au click sur le button "Ajouter"
+$annuler.onclick = annuler; // annuler le dernier élément ajouter au click
+
+afficherListe(); 
+
+
